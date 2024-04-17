115cae684ec32747bb70e5a3effbb4554bea53db
the7ag <mohamed.dead1543@gmail.com>
2024-04-18 00:20:10 +0200

    test

---
+++ DB/test.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL+"?sslmode=require",
+  });
+pool.connect((err)=>
+{
+    if (err) throw err
+    console.log('Connect to postgresql successfully!')
+})
+module.exports = pool
