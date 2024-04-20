d0300f432d4495effaed0ed5bc945fd28fc65372
the7ag <mohamed.dead1543@gmail.com>
2024-04-21 00:40:30 +0200

    Updated

--- API/test.py
+++ API/test.py
@@ -12,3 +12,9 @@ def process_data():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+
+
+
+
+
--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express()
 const port = 3000
@@ -245,6 +247,20 @@ app.post('/rating', async (req, res) => {
         console.error('Error during rating:', err);
         res.status(500).send('An error occurred during rating');
     }
+});
+app.post('/recommend', (req, res) => {
+  const flutterData = req.body;
+
+  // Send data to Python script
+  axios.post('http://127.0.0.1:5000/process-data', flutterData)
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data); // Optionally, send response back to Flutter
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error processing data');
+    });
 });
 
   app.get('/home', (req, res) => {
